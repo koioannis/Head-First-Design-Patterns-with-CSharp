91cb9ad2a04e9d40be6af73367007b74ecd36e21
Ioannis Kontopoulos <giannisproff733@hotmail.com>
2021-02-25 13:35:45 +0200

    added: chapter 05 - the singleton pattern

---
+++ Chapter 05 - The Singleton Pattern/ChocolateBoilerSingleton.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Chapter_05___The_Singleton_Pattern
+{
+    public sealed class ChocolateBoilerSingleton
+    {
+        private static readonly Lazy<ChocolateBoilerSingleton> Lazy =
+            new Lazy<ChocolateBoilerSingleton>(() => new ChocolateBoilerSingleton());
+        
+        public static ChocolateBoilerSingleton Instance => Lazy.Value;
+
+        private bool IsEmpty { get; set; }
+        
+        private bool IsBoiled { get; set; }
+
+        private ChocolateBoilerSingleton()
+        {
+            IsEmpty = true;
+            IsBoiled = false;
+        }
+
+        public void Fill()
+        {
+            if (!IsEmpty)
+            {
+                Console.WriteLine("Already filled!");
+                return;
+            }
+            
+            Console.WriteLine("Filling");
+            
+            IsEmpty = false;
+            IsBoiled = false;
+        }
+
+        public void Drain()
+        {
+            if (!IsEmpty && IsBoiled)
+                IsEmpty = true;
+            
+            Console.WriteLine("Draining");
+        }
+
+        public void Boil()
+        {
+            if (IsEmpty || IsBoiled) return;
+
+            IsBoiled = true;
+            Console.WriteLine("Boiling");
+        }
+    }
+}
---
+++ Chapter 05 - The Singleton Pattern/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Chapter_05___The_Singleton_Pattern
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var instance1 = ChocolateBoilerSingleton.Instance;
+            var instance2 = ChocolateBoilerSingleton.Instance;
+            
+            instance1.Fill();
+            /* we can see that it is already filled,
+             since instance 1 and instance2 share the same instance */
+            instance2.Fill();
+            
+            instance2.Boil();
+            
+            instance1.Drain();
+        }
+    }
+}
